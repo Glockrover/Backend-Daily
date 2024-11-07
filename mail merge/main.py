b96ebf5d18ecf15a7d8afe62ce5e6dc1e87522cd
wtc <wtc@pop-os.localdomain>
2024-11-07 17:22:33 +0200

    Almost Done

--- mail merge/main.py
+++ mail merge/main.py
@@ -6,3 +6,35 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+def main():
+    try:
+        file_name = "./Input/Names/inivited_names.txt"
+        file = open_txt(file_name)
+        letter = "./Input/letters/starting_letter.docx"
+        message = open_letter(letter)
+        for names in file:
+            x = message.replace("[name]", names)
+            with open("ReadyToSend.txt", "w") as file:
+                file.writelines(x)
+    except:
+        raise FileNotFoundError
+
+def open_txt(text_file):
+    names = list()
+    with open(text_file, 'r') as file:
+        contents = file.readlines()
+        for name in contents:
+            names.append(name.strip())
+    return names
+
+def open_letter(letter_docx):
+    letter = ""
+    with open(letter_docx) as file:
+        content = file.readlines()
+        for words in content:
+            letter += words
+    return letter
+
+if __name__ == "__main__":
+    main()
